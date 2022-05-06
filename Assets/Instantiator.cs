c0877a00eff2b129f73fcad42879562409e542e3
BautyGarcia <bautistaroccogarcia@gmail.com>
2022-05-06 14:28:56 -0300

    BlowCubes

---
+++ Assets/Instantiator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    public GameObject objectClone;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateCube(){
+        int count = 0;
+
+        while (count < 3)
+        {
+            Instantiate(objectClone);
+            
+            count++;
+        }
+
+    }
+}
